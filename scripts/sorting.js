b872068062708e2a3fad7d98a34bf7411a6112ab
Atul Saini <atulsaini9886@gmail.com>
2021-11-22 20:51:08 +0530

    added bars through javascript

---
+++ scripts/sorting.js
@@ -0,0 +1,27 @@
+var bars_section = document.getElementById("bars-section");
+var new_array_btn = document.getElementById("new-array-btn");
+
+function generateNewArray() {
+  console.log("clicked !!!!");
+  const numbers = [];
+  const elements = [];
+
+  for (let i = 0; i < 100; i++) {
+    numbers[i] = Math.floor(Math.random() * 91) + 1;
+  }
+
+  for (let i = 0; i < 100; i++) {
+    elements[i] = document.createElement("DIV");
+    elements[i].classList.add("array-bars");
+    elements[i].style.marginRight = "1em";
+    let newHeight = numbers[i].toString();
+
+    elements[i].innerHTML = newHeight;
+
+    elements[i].style.minHeight = newHeight + "%";
+    bars_section.appendChild(elements[i]);
+  }
+}
+
+new_array_btn.addEventListener("click", generateNewArray);
+generateNewArray();
