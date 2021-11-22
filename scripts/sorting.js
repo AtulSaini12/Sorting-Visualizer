ef4c00ce348ccda64b7ce49d83efd8c366137c5b
Atul Saini <atulsaini9886@gmail.com>
2021-11-22 21:24:17 +0530

    Added new array button functionality

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -1,25 +1,23 @@
 var bars_section = document.getElementById("bars-section");
 var new_array_btn = document.getElementById("new-array-btn");
+var numbers = [];
+var elements = [];
+
+for (let i = 0; i < 100; i++) {
+  elements[i] = document.createElement("DIV");
+  elements[i].classList.add("array-bars");
+  elements[i].style.marginRight = "0.3em";
+  elements[i].style.width = "1.5em";
+  bars_section.appendChild(elements[i]);
+}
 
 function generateNewArray() {
   console.log("clicked !!!!");
-  const numbers = [];
-  const elements = [];
 
   for (let i = 0; i < 100; i++) {
     numbers[i] = Math.floor(Math.random() * 91) + 1;
-  }
-
-  for (let i = 0; i < 100; i++) {
-    elements[i] = document.createElement("DIV");
-    elements[i].classList.add("array-bars");
-    elements[i].style.marginRight = "0.3em";
     let newHeight = numbers[i].toString();
-
-    elements[i].innerHTML = newHeight;
-
-    elements[i].style.minHeight = newHeight + "%";
-    bars_section.appendChild(elements[i]);
+    elements[i].style.height = newHeight + "%";
   }
 }
 
