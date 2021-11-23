3052f8a20f974650efbeb3e17f62bea6768013bf
Atul Saini <atulsaini9886@gmail.com>
2021-11-23 19:21:31 +0530

    Added Selection-sort Algorithm

---
+++ scripts/selection_sort.js
@@ -0,0 +1,31 @@
+async function selection_sort() {
+  console.log("in selection");
+  const bars = document.querySelectorAll(".array-bars");
+
+  for (let i = 0; i < bars.length - 1; i++) {
+    let minBarInd = i;
+    bars[minBarInd].style.background = "blue";
+    for (let j = i + 1; j < bars.length; j++) {
+      bars[j].style.background = "red";
+      if (
+        parseInt(bars[j].style.height) < parseInt(bars[minBarInd].style.height)
+      ) {
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve(), 100;
+          })
+        );
+
+        swap(bars[minBarInd], bars[j]);
+      }
+      bars[j].style.background = "rgb(179, 79, 179)";
+    }
+    bars[i].style.background = "green";
+  }
+  bars[bars.length - 1].style.background = "green";
+}
+
+const selection_btn = document.querySelector("#selection-btn");
+selection_btn.addEventListener("click", async function () {
+  await selection_sort();
+});
